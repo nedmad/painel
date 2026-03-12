532652401ce207a66dce7fc44d532f50bee98444
nedmad <matheusmned@gmail.com>
2026-03-12 16:39:51 -0300

    correção sintaxe  português

--- src/components/painel/totais_atend/totaisAtend.tsx
+++ src/components/painel/totais_atend/totaisAtend.tsx
@@ -14,7 +14,7 @@ export default function TotaisAtend({atendimentos,user}:AtendiProp){
         <div className="flex justify-center gap-5">
             <div className="border border-gray-300 rounded px-4 py-3 font-bold text-green-500 text-xl"> Online: {userOn.length}</div>
             <div className="border border-gray-300 rounded px-4 py-3 font-bold text-xl text-gray-500"> Offline: {userOff.length}</div>
-            <div className="rounded px-2 py-3 bg-blue-500 font-bold text-white text-xl"> Atendimentos simultânios: { atendimentos.totalAtendimentos.value}</div>
+            <div className="rounded px-2 py-3 bg-blue-500 font-bold text-white text-xl"> Atendimentos simultâneos: { atendimentos.totalAtendimentos.value}</div>
             <div className={`rounded px-2 py-3 ${atendimentos.totalEspera.value > 0?"bg-amber-300":"bg-gray-300"} font-bold text-white text-xl`}> Atendimentos na espera: { atendimentos?.totalEspera?.value}</div>
 
 
